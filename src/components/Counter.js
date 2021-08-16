d093cede1e6a75d8dc8b96247c248fe72f67bc8a
Aleksander Ivanov <62203063+japsap@users.noreply.github.com>
2021-08-17 00:15:48 +0300

    Add files via upload

---
+++ src/components/Counter.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+
+//btn btn-secondary btn-sm
+//badge m-2 badge-
+export class Counte extends Component {
+    state = {
+        count: this.props.value
+    }
+    incrementSpan(){
+        this.setState({ count: this.state.count + 1})
+    }
+    render() {
+        return (
+            <div> 
+                <span className={this.primaryWarning()}>{this.formatCount()}</span>
+                <button 
+                    className="btn btn-secondary btn-sm"
+                    onClick={() => {this.incrementSpan()}}>
+                    Increment
+                </button>
+                <button 
+                    className="btn btn-danger m-2 btn-sm"
+                    onClick={() => {this.props.onDelete(this.props.id)}}>
+                    Delete
+                </button>
+            </div>
+        )
+    }
+    formatCount(){
+        const { count } = this.state;
+        return count === 0 ? "Zero" : count
+    }
+    primaryWarning(){
+        let classes = "badge m-2 badge-";
+        classes += this.state.count === 0 ? "warning" : "primary";
+        return classes
+    }
+}
+
+
+export default Counte
